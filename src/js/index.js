7b47eea0bbda393da87b97054121f95ecae0df00
luciano588 <luciano588.alm@gmail.com>
2020-07-15 00:05:17 +0000

    first

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,15 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+for (let i = 0; i < pronoun.length; i++) {
+    for(let b=0; b< adj.length; b++) {
+        for (let c=0; c< noun.length; c++){
+console.log (pronoun[i]+adj[b]+noun[c]+".com");
+  
+  
+    document.querySelector("#domain").innerHTML = 
+    pronoun[i]+adj[b]+noun[c]+".com"
 };
