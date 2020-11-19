86d1204f82ac2fc86cfcb8d3afce5858418f597c
luciano588 <luciano588.alm@gmail.com>
2020-11-19 00:12:13 +0000

    switch function

--- src/js/index.js
+++ src/js/index.js
@@ -5,13 +5,24 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
+function getcolor(person) {
+  switch (person) {
+    case "Colonel Mustard":
+      return "yellow";
+    case "Mr.Green":
+      return "green";
+    case "Mrs.Peacock":
+      return "pink";
+  }
+}
+
 window.onload = function() {
   let who = [
     "Colonel Mustard",
     "Professor Plum",
-    "Mr. Green",
-    "Mrs. White",
-    "Mrs. Peacock"
+    "Mr.Green",
+    "Mrs.White",
+    "Mrs.Peacock"
   ];
   let where = ["Hall", "Kitchen"];
   let whepon = [
@@ -28,8 +39,10 @@ window.onload = function() {
     for (let b = 0; b < where.length; b++) {
       for (let c = 0; c < whepon.length; c++) {
         finalstring +=
+          `<span id='personid' style="color:${getcolor(who[i])}">` +
           who[i] +
-          " killed Miss Scarlet at " +
+          "</span>" +
+          " killed <em> Miss Scarlet </em> at " +
           where[b] +
           " Using " +
           whepon[c] +
